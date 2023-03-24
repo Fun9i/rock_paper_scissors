84d121fa1583a1ac7f42a4c443f0ef808e685701
fun9i <92895086+Fun9i@users.noreply.github.com>
2023-03-24 19:17:24 +0100

    Changes to be committed: 	modified:   rock_paper_scissors.js
    		Added prompt input for usersSelection and made strings toLowerCase

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -9,15 +9,18 @@ const selections = ['Rock', 'Paper', 'Scissors'];
 function computerChoice() {
 	let rnd = Math.floor(Math.random() * selections.length);
 
-    if (rnd === 0) {
-        console.log('Rock');
-        return 'ROCK';
-    } else if (rnd === 1) {
-        console.log('Paper');
-        return 'PAPER';
-    } else if (rnd === 2) {
-        console.log('Scissors');
-        return 'SCISSORS';
-    }
+	if (rnd === 0) {
+		console.log('rock');
+		return 'rock';
+	} else if (rnd === 1) {
+		console.log('paper');
+		return 'paper';
+	} else if (rnd === 2) {
+		console.log('scissors');
+		return 'scissors';
+	}
 }
 computerChoice();
+
+userSelection = prompt('Input your selectin(rock, paper or scissors)', '');
+console.log(userSelection.toLowerCase());
