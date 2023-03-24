6431188b46d5bd57a9e26e4bdeff3a693bb36154
fun9i <92895086+Fun9i@users.noreply.github.com>
2023-03-24 20:28:52 +0100

    Changes to be committed: 	modified:   Added play() function in rock_paper_scissors.js

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,22 +1,16 @@
 let computerScore = 0;
 let userScore = 0;
 
-let computerSelection;
-let userSelection;
-
 const selections = ['Rock', 'Paper', 'Scissors'];
 
 function computerChoice() {
 	let rnd = Math.floor(Math.random() * selections.length);
 
 	if (rnd === 0) {
-		console.log('rock');
 		return 'rock';
 	} else if (rnd === 1) {
-		console.log('paper');
 		return 'paper';
 	} else if (rnd === 2) {
-		console.log('scissors');
 		return 'scissors';
 	}
 }
@@ -24,6 +18,8 @@ function computerChoice() {
 function play(computerSelection, userSelection) {
     computerSelection = computerChoice()
     userSelection = prompt('Input your selection(rock, paper or scissors)', '');
-    lowerCaseSelection = userSelection.toLowerCase(); 
+    lowerCaseSelection = userSelection.toLowerCase();
+    console.log(computerSelection);
+    console.log('\n');
     console.log(lowerCaseSelection);
 }
